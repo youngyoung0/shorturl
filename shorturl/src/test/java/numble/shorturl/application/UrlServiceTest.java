8f1841d7abe99d3d0e8b32a8e432926149e072ce
youngyoung0 <taeyoung6873@gmail.com>
2023-05-05 03:49:24 +0900

    feature: url 통계 API 개발중

--- shorturl/src/test/java/numble/shorturl/application/UrlServiceTest.java
+++ shorturl/src/test/java/numble/shorturl/application/UrlServiceTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -32,13 +33,11 @@ class UrlServiceTest {
                 .andExpect(status().isOk());
     }
 
-//    @Test
-//    @DisplayName("인코딩_URL_호출")
-//    public void callUrl(){
-//        mockMvc.perform(MockMvcRequestBuilders
-//                .get("/B"))
-//                .andExpect(status().isOk());
-//    }
+    @Test
+    public void subString(){
+        String encodingUrl = "localhost:8080/D+";
+        System.out.println(encodingUrl.charAt(encodingUrl.length() - 1));
+    }
 
     @Test
     @DisplayName("URL 디코딩")
